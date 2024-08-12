7800bdd3e0846582b69225a3ab70bdd65d581f59
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-08-12 20:09:26 +0530

    JoinWAves-set css bugs

--- src/app/sections/JoinWAVES.tsx
+++ src/app/sections/JoinWAVES.tsx
@@ -8,7 +8,9 @@ function JoinWAVES() {
       <img src="/assets/Styleguide.png" alt="empty" className="" />
       <div className='relative bottom-[241px]'>
       <h1 className="text-center text-white text-6xl font-bold mt-10 mb-6">Secure your spot at WAVES 2024</h1>
-      <Button className='relative left-[690px] mt-[50px] w-[200px] h-[53px] bg-none text-white text-[18px] font-bold mb-5'>Join WAVES</Button>
+      <button className='relative left-[600px] mt-[10px] w-[250px] h-[63px] border border-gray-300 rounded-xl text-white bg-none text-[30px] font-bold hover:bg-white hover:text-blue-500'>
+  Join WAVES
+</button>
       </div>
 
     </div>

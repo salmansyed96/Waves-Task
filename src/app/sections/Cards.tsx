b67605a9a07bd2f317aedf79ff4caeef74484503
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-08-12 15:19:40 +0530

    cards-bugs css fix

--- src/app/sections/Cards.tsx
+++ src/app/sections/Cards.tsx
@@ -6,8 +6,8 @@ export const Cards = () => {
                     className="  w-[400px] bg-white border border-[#edecec] rounded-lg shadow-lg p-4 flex flex-col items-center justify-center text-[#3F4654]"
                     style={{ borderWidth: '0.5px' }} >
                     <div className="flex">
-                        <img src="/assets/loca.png" alt="empty" className="mr-6" />
-                        <div>
+                        <img   src="/assets/loca.png" alt="empty" className="mr-6 relative right-[50px]" />
+                        <div className="mt-5">
                             <h2 className="text-xl font-semibold mb-2">Where :</h2>
                             <p>Goa.</p>
                         </div>
@@ -16,11 +16,11 @@ export const Cards = () => {
             </div>
 
             <div
-                className="w-100 h-35 bg-white border border-[#edecec] rounded-lg shadow-lg p-4 flex flex-col items-center justify-center text-[#3F4654]"
+                className="w-[400px] h-[130px] bg-white border border-[#edecec] rounded-lg shadow-lg p-4 flex flex-col items-center justify-center text-[#3F4654]"
                 style={{ borderWidth: '0.5px' }}>
 
-                <div className="flex">
-                    <img src="/assets/clc.png" alt="empty" className="mr-6" />
+                <div className="flex mt-5">
+                    <img src="/assets/clc.png" alt="empty" className="mr-6 relative right-[10px] bottom-[9px]" />
                     <div>
                         <h2 className="text-xl font-semibold mb-2">When</h2>
                         <p>November  21-24, 2024</p>

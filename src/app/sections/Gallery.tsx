59cc6b93a4da04ad077db01f3ac2673536c42eef
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-08-16 17:48:13 +0530

    fix gride

--- src/app/sections/Gallery.tsx
+++ src/app/sections/Gallery.tsx
@@ -3,36 +3,42 @@ import React from 'react'
 function Gallery() {
   return (
     <div className="w-full h-[1018px] bg-[linear-gradient(to_right,_rgba(95,37,158,0.3),_rgba(173,109,244,0.3),_rgba(255,95,228,0.3),_rgba(255,255,255,0.3))]">
-        <div className='text-center'>
-        <h1 className="text-5xl font-bold text-[rgb(95,37,158)]">Gallery</h1>
+        <div className='text-center p-20'>
+        <h1 className="text-5xl font-bold text-[#5F259E]">Gallery</h1>
 <p className="text-lg font-semibold text-[#3F4654]">Get a sneak peek into what happened at the WAVES Summit</p>
 </div>
 
 <div className="container mx-auto">
     <div className="flex flex-wrap">
+        
         {/* First Row */}
-        <div className="w-full md:w-full bg-red-300 p-4">
-            <div className="w-full md:w-full">A</div>
-        </div>
-        {/* Second Row */}
-        <div className="w-full md:w-full bg-blue-300 p-4">
-            <div className="w-full md:w-full">B</div>
-        </div>
-        {/* Third Row */}
-        <div className="w-full md:w-4/12 bg-green-300 p-4">
-            C
-        </div>
-        {/* Fourth Row */}
-        <div className="w-full md:w-5/12 bg-yellow-300 p-4">
-            <div className="w-full md:w-full bg-purple-300 p-4">
-                <div className="w-full md:w-full">A</div>
-            </div>
-            <div className="flex">
-                <div className="w-full md:w-6/12 bg-pink-300 p-4">B</div>
-                <div className="w-full md:w-6/12 bg-orange-300 p-4">C</div>
-            </div>
-        </div>
+        <div className="w-full md:w-4/12 bg-pink-300 p-4 h-[320px]">
+        <img src="/assets/photo1.png" alt="empty" className="" />
+</div>
+
+<div className="w-full md:w-4/12 bg-yellow-300 p-4" style={{ position: 'relative', top: '0', left: '20px',height: '618px' }}>
+    B
+</div>
+
+<div className="w-[] md:w-4/12 bg-red-300 p-4 h-[345px]" style={{ position: 'relative', left: '43px' }}>
+    A
+</div>
+
+
+
+<div className="w-full md:w-4/12 bg-green-300 p-4" style={{ position: 'relative', bottom: '347px', height: '345px' }}>
+    
+    D
+</div>
+<div className="w-full md:w-4/12 bg-red-300 p-4" style={{ position: 'relative', left: '487px', bottom: '264px', width: '17%', height: '255px' }}>
+    F
+</div>
+<div className=" ml-[53px] gap-1 w-full md:w-4/12 bg-green-300 p-4" style={{ position: 'relative', left: '450px', bottom: '264px', width: '17%', height: '255px' }}>
+    g
+</div>
+
     </div>
+    
 </div>
 
 

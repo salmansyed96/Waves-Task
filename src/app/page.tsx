726f49686c7bb1cf766e81d205c12641aba1230a
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-08-19 12:07:57 +0530

    fix deplying err

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,21 +4,15 @@ import CountdownTimer from "./sections/Countdown";
 import { About } from "./sections/ABout_Waves";
 import { Verticals } from "./sections/Verticals";
 import { Cards } from "./sections/Cards";
-import { CarouselSize } from "./sections/CarouselSize";
 import HorizontalScroll from "./sections/HorizontalScroll";
-
-import CardMedia from "./sections/Card";
-import CardSlider from "@/components/ui/Card Slider Component";
 import Activities from "./sections/Activities";
 import JoinWAVES from "./sections/JoinWAVES";
 import NewsUpdate from "./sections/News&Updates";
-import { Accordion } from "@radix-ui/react-accordion";
 import { AccordionDemo } from "./sections/accordion";
 import Footer from "./sections/Footer";
 import Gallery from "./sections/Gallery";
 import Media from "./sections/media";
 import ContactForm from "./sections/Contact";
-import BoutiqueHotelWebsite from "./sections/test-card";
 // import CardSlider from "@/components/ui/Card Slider Component";
 
 
@@ -27,26 +21,19 @@ export default function page() {
     <div>
       <Hero />
       <CountdownTimer targetDate={new Date('2024-08-30T00:00:00')} />
-      <About/>
-      <Verticals/>
-      <Cards/>
+      <About />
+      <Verticals />
+      <Cards />
       {/* <CarouselSize/> */}
-      <HorizontalScroll/>
-      <Activities/>
-      
-      <JoinWAVES/>
-      
-      <NewsUpdate/>
-      <AccordionDemo/>
-      <Gallery/>
-      <Media/>
-      <ContactForm/>
-      
-
-      
-      
-      
-      <Footer/>
+      <HorizontalScroll />
+      <Activities />
+      <JoinWAVES />
+      <NewsUpdate />
+      <AccordionDemo />
+      <Gallery />
+      <Media />
+      <ContactForm />
+      <Footer />
     </div>
   )
 }
--- src/app/sections/CarouselSize.tsx
+++ src/app/sections/CarouselSize.tsx
@@ -1,40 +1,40 @@
-import * as React from "react"
+// import * as React from "react"
 
-import { Card, CardContent } from "@/components/ui/Card"
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel"
+// import {  } from "@/components/ui/Card"
+// import {
+//   Carousel,
+//   CarouselContent,
+//   CarouselItem,
+//   CarouselNext,
+//   CarouselPrevious,
+// } from "@/components/ui/carousel"
 
-export function CarouselSize() {
-  return (
-    <Carousel
-      opts={{
-        align: "start",
-      }}
-      className="w-6 "
-    >
-      <CarouselContent>
-        {Array.from({ length: 5 }).map((_, index) => (
-          <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-            <div className="p-1">
-              <Card>
-                <CardContent className="flex aspect-square items-center justify-center p-6">
-                  <span className="text-3xl font-semibold">{index + 1}
+// export function CarouselSize() {
+//   return (
+//     <Carousel
+//       opts={{
+//         align: "start",
+//       }}
+//       className="w-6 "
+//     >
+//       <CarouselContent>
+//         {Array.from({ length: 5 }).map((_, index) => (
+//           <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+//             <div className="p-1">
+//               {/* <Card>
+//                 <CardContent className="flex aspect-square items-center justify-center p-6">
+//                   <span className="text-3xl font-semibold">{index + 1}
                     
-                  </span>
-                  <img src="/assets/clc.png" alt="empty" className="mr-6" />
-                </CardContent>
-              </Card>
-            </div>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <CarouselPrevious />
-      <CarouselNext />
-    </Carousel>
-  )
-}
+//                   </span>
+//                   <img src="/assets/clc.png" alt="empty" className="mr-6" />
+//                 </CardContent>
+//               </Card> */}
+//             </div>
+//           </CarouselItem>
+//         ))}
+//       </CarouselContent>
+//       <CarouselPrevious />
+//       <CarouselNext />
+//     </Carousel>
+//   )
+// }

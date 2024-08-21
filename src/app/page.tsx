4783da80c32e75a967de26a0d20e3494dd52df3d
Syed Mohammad Salman <124539970+salmansyed96@users.noreply.github.com>
2024-08-21 13:03:32 +0530

    fix bugs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -23,7 +23,6 @@ export default function page() {
       <About />
       <Verticals />
       <Cards />
-      {/* <CarouselSize/> */}
       <HorizontalScroll />
       <Activities />
       <JoinWAVES />
